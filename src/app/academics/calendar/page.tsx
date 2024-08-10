004c108198f5009c8a072c34d37e63e5469f6a98
ZaneFerns360 <zane.ferns360@gmail.com>
2024-08-10 21:43:39 +0530

    changes

---
+++ src/app/academics/calendar/page.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const PDFViewerPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col bg-gray-100">
+      <header className="bg-gradient-to-br from-[#001f3f] to-[#003366] p-6 text-center text-white">
+        <h1 className="text-2xl font-bold">Academic Calendar</h1>
+      </header>
+
+      <main className="container mx-auto my-8 flex-grow p-4">
+        <div className="mb-8 rounded-lg bg-white p-6 shadow-md">
+          <h2 className="mb-4 text-2xl font-semibold">Academic Calendar</h2>
+          <div className="h-[800px] w-full">
+            {' '}
+            {/* Adjust height as needed */}
+            <object
+              data="/Academic-Calendar-2023-34.pdf"
+              type="application/pdf"
+              width="100%"
+              height="100%"
+              className="border border-gray-300"
+            >
+              <p>
+                Unable to display PDF file.
+                <a
+                  href="/Academic-Calendar-2023-34.pdf"
+                  className="text-blue-600 hover:underline"
+                >
+                  Download
+                </a>{' '}
+                instead.
+              </p>
+            </object>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default PDFViewerPage
---
+++ src/app/students/grievance-policy/page.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const PDFViewerPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col bg-gray-100">
+      <header className="bg-gradient-to-br from-[#001f3f] to-[#003366] p-6 text-center text-white">
+        <h1 className="text-2xl font-bold">
+          STUDENT GRIEVANCE REDRESSAL POLICY
+        </h1>
+      </header>
+
+      <main className="container mx-auto my-8 flex-grow p-4">
+        <div className="mb-8 rounded-lg bg-white p-6 shadow-md">
+          <h2 className="mb-4 text-2xl font-semibold">
+            STUDENT GRIEVANCE REDRESSAL POLICY
+          </h2>
+          <div className="h-[800px] w-full">
+            {' '}
+            {/* Adjust height as needed */}
+            <object
+              data="/grievance_policy.pdf"
+              type="application/pdf"
+              width="100%"
+              height="100%"
+              className="border border-gray-300"
+            >
+              <p>
+                Unable to display PDF file.
+                <a
+                  href="/grievance_policy.pdf"
+                  className="text-blue-600 hover:underline"
+                >
+                  Download
+                </a>{' '}
+                instead.
+              </p>
+            </object>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default PDFViewerPage
