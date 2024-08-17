35c09435a6bfedf7114ac69681172d7758b862a6
chris <chris@localhost.localdomain>
2024-08-17 21:53:28 +0530

    fix

--- src/app/about/about-crce/page.tsx
+++ src/app/about/about-crce/page.tsx
@@ -1,16 +1,53 @@
-
 import React from 'react'
 
-
-
 export default function Page() {
-
   return (
-    <main className="flex min-h-screen flex-col gap-4 items-center justify-center">
-      <div className='bg-gradient-to-b from-gray-50 to-[#E5F0FF] w-full h-fit '>
-        
-
-      </div>
+    <main className="flex min-h-screen w-full flex-col items-center justify-center gap-4 bg-gradient-to-b from-gray-50 to-[#E5F0FF]">
+      <div className="h-full w-full bg-red-800 ">f</div>
+        <div className="container mx-auto rounded-xl bg-white p-6 shadow-lg">
+          Located in a picturesque environment in the heart of the Mumbai city,
+          Fr.Conceicao Rodrigues College of Engineering (Fr.CRCE) is one of the
+          renowned, premier and sought-after private Engineering colleges. The
+          Institute is dedicated to making a difference in Engineering
+          education, with its exclusive approach towards the ongoing momentum of
+          trends in technology and holistic development.
+          <br className="mb-2" />
+          Beginning with an orphanage (BalBhavan) and a trade school in
+          carpentry, the Agnel Ashram (set up in 1957 at Bandra, Mumbai) has
+          today grown into a full-fledged Technical Complex. The Institute today
+          proudly bears the founder's name as a fitting tribute to his
+          impassioned faith in highly-qualified and fully-trained engineers and
+          technicians in the service of the nation.
+          <br className="mb-2" />
+          The Institute started with only one programme in Production
+          Engineering in 1984 with an intake capacity of 60 students. In 1987,
+          the programme in Electronics Engineering was started with an intake
+          capacity of 60 students followed by Computer Engineering in 1991 with
+          an intake capacity of 60 students. In 2001, the programme in
+          Information Technology was started with an intake capacity of 30
+          students which was later increased to 60 in 2010. The College started
+          offering Master’s Degree in Electronics Engineering and Mechanical
+          Engineering in the year 2005, each having an intake capacity of 18.
+          Doctoral programs in Electronics Engineering and Mechanical
+          Engineering commenced in 2014 and 2015 respectively. From the year
+          2019, a new Undergraduate degree programme in Mechanical Engineering
+          has been started with an intake capacity of 60. Also, the BE
+          undergraduate programme in Electronics Engineering has been changed to
+          Electronics and Computer Science. The Undergraduate programme in
+          Information Technology has been merged with Computer Engineering. From
+          the year 2020, college is offering new undergraduate programme in the
+          emerging area of ‘AI & Data Science’. Also, Doctoral program in
+          Computer Engineering started in the year 2023.
+          <br className="mb-2" />
+          The Institute has been consistently ranked highly in major surveys
+          like TOI, INDIA TODAY, Career 360, The Week and Education World to
+          name a few. College is ranked by NIRF in 2017 and 2020. Based on the
+          Outcome-Based Education (OBE) parameters and criteria, three
+          programmes (Computer, Electronics and Production Engineering) of the
+          institute have been accredited by the National Board of Accreditation
+          (NBA-India) since 2017-2018. The college is also a NAAC accredited
+          with grade 'A
+        </div>
     </main>
   )
 }
