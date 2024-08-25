1cc91982f612374e04243c7a33af92879187b7ff
ZaneFerns360 <zane.ferns360@gmail.com>
2024-08-25 20:56:56 +0530

    changes

---
+++ src/app/about/mou/page.tsx
@@ -0,0 +1,241 @@
+import React from 'react'
+import { Zilla_Slab } from 'next/font/google'
+
+const zilla = Zilla_Slab({
+  weight: ['300', '500', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+const MOUCollaborationPage = () => {
+  return (
+    <div className="container mx-auto w-full px-4 py-16 pt-48">
+      <h1
+        className={`${zilla.className} mb-6 text-4xl font-bold text-[#001f3f] md:text-5xl lg:text-7xl`}
+      >
+        MoU & Collaborations
+      </h1>
+      <div className="mb-16">
+        <p className="mb-8 text-gray-700">
+          The institute has signed various Memorandums of Understanding with
+          several Companies, Foreign Universities, Institutes of national and
+          international repute, and Technology Providers to provide avenues for
+          faculty exchange, student exchange, internships, field trips,
+          on-the-job trainings, research, as well as other academic and
+          co-curricular activities.
+        </p>
+
+        {mouData.map((mou, index) => (
+          <div key={index} className="mb-10">
+            <h2
+              className={`${zilla.className} text-3xl font-bold text-[#001f3f] md:text-4xl`}
+            >
+              {mou.organization}
+            </h2>
+            <div className="rounded-lg bg-gray-100 p-6 shadow-lg">
+              <p className="mb-4 text-xl font-semibold text-[#4a90e2]">
+                {mou.description}
+              </p>
+              <a
+                href={mou.link}
+                className="text-blue-600 underline"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {mou.linkText}
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+const mouData = [
+  {
+    organization: 'Brillmax Private Limited',
+    description:
+      'The purpose of this MoU is to perform the collaborative programs for students and faculty members.',
+    link: 'https://brillmax.com/',
+    linkText: 'https://brillmax.com/',
+  },
+  {
+    organization: 'Cloud Counselage',
+    description:
+      'The purpose of this MoU is to perform the collaborative programs for students and faculty members, to arrange industrial trainings and visits and to provide internships and placements to the students within FR.CRCE.',
+    link: 'https://www.cloudcounselage.com/',
+    linkText: 'https://www.cloudcounselage.com/',
+  },
+  {
+    organization: 'Conbuss Online Manufacturing Pvt. Ltd',
+    description:
+      'The purpose of this MoU is to perform the collaborative programs for students and faculty members in the areas of mutual interest, to provide internships to the students within FR.CRCE.',
+    link: '',
+    linkText: '',
+  },
+  {
+    organization: 'Internshala',
+    description:
+      'The purpose of this MoU is to provide Internship opportunities to the students within Fr.CRCE.',
+    link: 'https://trainings.internshala.com/',
+    linkText: 'https://trainings.internshala.com/',
+  },
+  {
+    organization: 'Hyfunn',
+    description:
+      'The purpose of this MoU is to provide digital services for a term of 24 months and to support and sustain the management of activities for various engineering departments.',
+    link: 'https://hyfunnverse.com/',
+    linkText: 'https://hyfunnverse.com/',
+  },
+  {
+    organization: 'Studenting Era',
+    description:
+      'The purpose of this MoU is to provide free online trainings and services to the registered student and faculty members helping them to improve the skills, competencies, and employability of students.',
+    link: '',
+    linkText: '',
+  },
+  {
+    organization: 'BUDSTA Analytics and Insights Pvt. Ltd.',
+    description:
+      'The purpose of this MoU is to provide internship opportunities to third year Engineering students within Fr.CRCE.',
+    link: '',
+    linkText: '',
+  },
+  {
+    organization: 'Bennett University',
+    description:
+      'The purpose of this MoU is to provide support to the projects of students in terms of manpower, local support and infrastructure.',
+    link: 'https://www.bennett.edu.in/',
+    linkText: 'https://www.bennett.edu.in/',
+  },
+  {
+    organization: 'Amazon Web Services (AWS)',
+    description:
+      'The purpose of this MoU is to provide guidance to students regarding the cloud usage in their project.',
+    link: 'https://aws.amazon.com/training/',
+    linkText: 'https://aws.amazon.com/training/',
+  },
+  {
+    organization: 'Tata Consultancy Services (TCS)',
+    description:
+      'The purpose of this MoU is to conduct workshops for students, to conduct faculty development programs, and to provide internships to the students.',
+    link: 'https://www.tcs.com/',
+    linkText: 'https://www.tcs.com/',
+  },
+  {
+    organization: 'Christiani Sharpline Technical Training',
+    description:
+      'The purpose of this MoU is to provide collaborative programs with both the organizations in the areas of interest.',
+    link: 'http://www.cstt.in',
+    linkText: 'http://www.cstt.in',
+  },
+  {
+    organization: 'D-LINK',
+    description:
+      'The purpose of this MoU is to provide the training to the students of Fr.CRCE.',
+    link: 'https://in.dlink.com/en/consumer',
+    linkText: 'https://in.dlink.com/en/consumer',
+  },
+  {
+    organization: 'Synergy Consultants Pvt. Ltd.',
+    description:
+      'The purpose of this MoU is to carry out the projects jointly with Fr.CRCE and SCPL.',
+    link: '',
+    linkText: '',
+  },
+  {
+    organization: 'University of Texas at EL Paso',
+    description:
+      'The purpose of this MoU is to promote the interest of teaching and research activities.',
+    link: 'https://www.utep.edu/',
+    linkText: 'https://www.utep.edu/',
+  },
+  {
+    organization: 'United Modelling & Simulation',
+    description:
+      "The purpose of this MoU is to provide the guidance for consultancy and sponsorship to the student's projects.",
+    link: 'http://www.unimodsim.com/',
+    linkText: 'http://www.unimodsim.com/',
+  },
+  {
+    organization: 'L. D. College of Engineering',
+    description:
+      'The purpose of this MoU is to provide internship opportunities to the students within Fr.CRCE and to facilitate the use of Siemens centre of excellence training facility to Fr.CRCE.',
+    link: 'http://ldce.ac.in/',
+    linkText: 'http://ldce.ac.in/',
+  },
+  {
+    organization: 'Eduvance',
+    description:
+      'The purpose of this MoU is to conduct the workshop for students, to conduct faculty development workshops and to provide the internships to the students.',
+    link: 'https://eduvance.in/',
+    linkText: 'https://eduvance.in/',
+  },
+  {
+    organization: 'CodeChef',
+    description:
+      'The purpose of this MoU is to help the students to improve their skills across a variety of topics and difficulty levels.',
+    link: 'https://www.codechef.com/',
+    linkText: 'https://www.codechef.com/',
+  },
+  {
+    organization: 'Crypto University',
+    description:
+      'The purpose of this MoU is to extend the cooperation for research, data sharing, and paper publication, and to provide the internships and live projects to the students.',
+    link: 'https://cryptowire.in/',
+    linkText: 'https://cryptowire.in/',
+  },
+  {
+    organization: 'EC Council',
+    description:
+      'The purpose of this MoU is to provide the courses related to the securities to the students.',
+    link: 'https://www.eccouncil.org/',
+    linkText: 'https://www.eccouncil.org/',
+  },
+  {
+    organization: 'Linux Professional Institute',
+    description:
+      'The purpose of this MoU is to conduct the workshops for students, to conduct faculty development workshops, and to provide the internships to the students.',
+    link: 'https://www.lpi.org/',
+    linkText: 'https://www.lpi.org/',
+  },
+  {
+    organization: 'Devfolio',
+    description:
+      'The purpose of this MoU is to provide the Hackathon Organizer with a cost-free SaaS platform.',
+    link: 'https://devfolio.co/',
+    linkText: 'https://devfolio.co/',
+  },
+  {
+    organization: 'Veermata Jijabai Technological Institute (VJTI)',
+    description:
+      'The purpose of this MoU is to stimulate and facilitate the development of collaborative and mutually beneficial programs which serve for technological upliftment of both campuses.',
+    link: 'https://vjti.ac.in/',
+    linkText: 'https://vjti.ac.in/',
+  },
+  {
+    organization: 'United Way',
+    description:
+      'The purpose of this MoU is to collaborate for participation in Mission Blue project activities.',
+    link: 'https://www.unitedwaymumbai.org/index.htm',
+    linkText: 'https://www.unitedwaymumbai.org/index.htm',
+  },
+  {
+    organization: 'Sardar Patel Institute of Technology (SPIT)',
+    description:
+      'The purpose of this MoU is to help each other to organize/conduct courses/training modules enabling students’ mobility across both the HEI for seamless integration of skills and experiences, deputing students/staff/faculty members for conduction of courses or research, involving each other in the revision of syllabus, teaching-learning processes, external quality audits, assessment and evaluation process, jointly organizing co-curricular and extra-curricular activities, undertaking collaborative research projects.',
+    link: 'https://www.spit.ac.in/',
+    linkText: 'https://www.spit.ac.in/',
+  },
+  {
+    organization: 'IIRS ISRO',
+    description:
+      'The purpose of this collaboration is to encourage students and teachers to actively participate in IIRS outreach programs.',
+    link: 'https://www.iirs.gov.in/',
+    linkText: 'https://www.iirs.gov.in/',
+  },
+]
+
+export default MOUCollaborationPage
---
+++ src/app/admission/postgraduate/page.tsx
@@ -0,0 +1,182 @@
+import React from 'react'
+import { Zilla_Slab } from 'next/font/google'
+import Navigation from '@/components/navigation'
+import { url } from 'inspector'
+
+const zilla = Zilla_Slab({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+const PostGraduateAdmissionsPage = () => {
+  const navigationItems = [
+    { label: 'Admission', url: '/' },
+    { label: 'Postgraduate', url: '/admission/postgraduate' },
+  ]
+
+  return (
+    <div className="flex h-fit w-full flex-col bg-gradient-to-b from-gray-50 to-[#E5F0FF] text-gray-900">
+      <div className="flex h-full w-full flex-col justify-center bg-gradient-to-br from-[#001f3f] to-[#003366] pt-36 md:flex-row">
+        <div className="flex w-full flex-col items-center justify-center p-8 pt-40 text-white md:w-2/3 md:p-16 md:pt-16">
+          <h1
+            className={`${zilla.className} mb-6 text-4xl font-bold md:text-5xl lg:text-7xl`}
+          >
+            Post Graduate Admissions
+          </h1>
+          <Navigation items={navigationItems} />
+        </div>
+      </div>
+
+      <div className="container mx-auto w-full px-4 py-16">
+        <div className="mb-16">
+          <h2
+            className={`${zilla.className} mb-6 text-3xl font-bold text-[#001f3f] md:text-4xl`}
+          >
+            Masters of Technology (M.Tech)
+          </h2>
+          <div className="rounded-lg bg-gray-100 p-6 shadow-lg">
+            <p className="mb-4 text-gray-700">
+              <strong>Masters of Technology (M.Tech)</strong>
+            </p>
+            <p className="mb-4 text-gray-700">
+              <strong>The college offers M.Tech Degree Course in:</strong>
+            </p>
+            <ul className="mb-4 list-inside list-disc text-gray-700">
+              <li>Computer Engineering</li>
+            </ul>
+            <p className="mb-4 text-gray-700">
+              Allotment of seats for the Masters Program will be done through
+              the centralized process.
+            </p>
+            <p className="mb-4 text-gray-700">
+              51% of the centralized admission seats are under Minority Quota.
+            </p>
+            <p className="mb-4 text-gray-700">
+              <strong>Eligibility:</strong> As per the Directives of the
+              DTE/Competent Authority/Regulatory Authority.
+            </p>
+            <ul className="mb-4 list-inside list-disc text-gray-700">
+              <li>Candidate should be an Indian National</li>
+              <li>
+                Passed Bachelor Degree in the relevant field of Engineering and
+                Technology from AICTE or Central or State Government approved
+                institutions or equivalent, with at least 50% marks (at least
+                45% marks in case of candidates of Backward Class categories,
+                Economically weaker section and Persons with Disability category
+                belonging to Maharashtra State)
+              </li>
+              <li>
+                Obtained score in the GATE examination in respective qualifying
+                branch valid for the current academic year.
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="mb-16">
+          <h2
+            className={`${zilla.className} mb-6 text-3xl font-bold text-[#001f3f] md:text-4xl`}
+          >
+            Documents Required
+          </h2>
+          <div className="rounded-lg bg-gray-100 p-6 shadow-lg">
+            <p className="mb-4 text-gray-700">
+              <strong>Vacant Seats after CAP Rounds</strong>
+              <br />
+              Vacant Seats after the Centralized Admission Process will be
+              filled by the Institute as per the guidelines provided in the
+              “Rules & Institute Information Brochure for the Academic year
+              2024-25” by DTE/ARA/Govt. of Maharashtra.
+            </p>
+            <p className="mb-4 text-gray-700">
+              <strong>Documents:</strong>
+            </p>
+            <ul className="mb-4 list-inside list-disc text-gray-700">
+              <li>
+                S.S.C, H.S.C./Diploma, B.E. Mark sheets (All Semesters) and BE
+                Degree Certificate.
+              </li>
+              <li>
+                Leaving/Transference Certificate from the previous College.
+              </li>
+              <li>
+                ‘Migration Certificate’ applicable to candidates who have
+                obtained B.E. Degree from other than the University of Mumbai.
+              </li>
+              <li>GATE Score Card.</li>
+              <li>
+                Certificate of Indian Nationality OR Indian Passport in the name
+                of the candidate OR College Leaving Certificate indicating the
+                Nationality as ‘Indian’ OR Birth Certificate issued by
+                appropriate authorities.
+              </li>
+              <li>
+                Maharashtra State Domicile Certificate OR Birth Certificate
+                indicating Birth Place in Maharashtra OR College Leaving
+                Certificate indicating Birth Place in Maharashtra.
+              </li>
+              <li>
+                Certificates of Proforma A (Sponsorship letter), E (for type C
+                candidates), F (for type D Candidates), F-1 (for P1 / P2 / P3
+                (OD) Candidates), F-2 for Physically handicapped Candidates
+              </li>
+              <li>
+                Undertaking in Proforma B (Sponsored Candidates), C Candidates
+                whose qualifying examination result not declared, D (for GATE
+                Candidates)
+              </li>
+              <li>Copy of Aadhaar Card</li>
+              <li>
+                Physical/Medical Fitness Certificate from a registered Medical
+                Practitioner.
+              </li>
+              <li>
+                Duly signed printout of Anti-ragging Affidavit submitted online
+                on www.antiragging.in by candidate and Parent. (Enter the
+                Application ID allotted by DTE/ARA CAP as Reference No.).
+              </li>
+              <li>
+                Any other relevant document specified by DTE/ARA/Govt. of
+                Maharashtra.
+              </li>
+              <li>
+                Proof of Income for the previous year issued by the appropriate
+                authorities such as Form No.16 or any other of parents for those
+                candidates who wish to claim the financial benefits i.e.
+                category candidates, EBC/Minority candidates from Govt. or from
+                any other organization if applicable.
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="rounded-lg bg-gray-100 p-6 shadow-lg">
+          <p className="mb-4 text-gray-700">
+            <a
+              href="/Mtech.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 underline"
+            >
+              M.Tech CAP Reporting Procedure (PDF)
+            </a>
+          </p>
+          <p className="text-gray-700">
+            <a
+              href="http://granth.fragnel.edu.in:5186/pinfo/meadm/MEMnu.php"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 underline"
+            >
+              M.Tech CAP Reporting Form
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PostGraduateAdmissionsPage
