e5dd19f4ae0ea4b504af3a1303f9f8b3e3ef835d
ZaneFerns360 <zane.ferns360@gmail.com>
2024-09-15 21:05:59 +0530

    changes

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,11 +1,9 @@
 'use client'
-
 import { useState } from 'react'
 import { ChevronRight, X } from 'lucide-react'
 
 export default function HeroSection() {
   const [isVisible, setIsVisible] = useState(false)
-
   const handleToggle = () => {
     setIsVisible(!isVisible)
   }
@@ -18,36 +16,45 @@ export default function HeroSection() {
           Your browser does not support the video tag.
         </video>
       </div>
-      <div className="relative z-10 flex min-h-screen items-center justify-end px-5 py-24 sm:mr-20 sm:mt-[-10] sm:justify-end">
-        <div
-          className={`absolute left-0 top-1/2 m-4 w-4/5 max-w-sm -translate-y-1/2 transform bg-white p-6 shadow-md transition-transform duration-500 sm:relative sm:mr-20 sm:mt-[-10] sm:w-full sm:max-w-md sm:translate-x-0 sm:rounded-lg ${isVisible ? 'translate-x-0' : '-translate-x-full'}`}
-        >
-          <div className="mb-4 flex items-center justify-between">
-            <h2 className="text-2xl font-medium leading-tight text-gray-900 sm:text-4xl">
-              Welcome new graduate students
-            </h2>
-            <button onClick={handleToggle} className="sm:hidden">
+      <div className="relative z-10 flex min-h-screen items-center justify-end px-5 py-24 sm:px-20">
+        <div className="sm:relative">
+          <div
+            className={`fixed left-0 top-1/2 w-4/5 max-w-sm -translate-y-1/2 transform rounded-r-lg bg-white p-6 shadow-md transition-transform duration-500 sm:relative sm:left-auto sm:top-auto sm:w-full sm:max-w-md sm:translate-x-0 sm:translate-y-0 sm:rounded-lg ${
+              isVisible ? 'translate-x-0' : '-translate-x-full'
+            }`}
+          >
+            <button
+              onClick={handleToggle}
+              className="absolute right-2 top-2 rounded-full bg-gray-200 p-2 sm:hidden"
+            >
               <X className="h-6 w-6 text-gray-900" />
             </button>
+            <div className="mb-4">
+              <h2 className="text-2xl font-medium leading-tight text-gray-900 sm:text-4xl">
+                Welcome new graduate students
+              </h2>
+            </div>
+            <p className="mb-2 text-lg text-gray-600">
+              Admission: Applications Opened
+            </p>
+            <p className="mb-6 text-lg font-bold text-gray-900">
+              Deadline: 8th July, 2024
+            </p>
+            <button className="w-full rounded-md bg-yellow-500 px-6 py-3 text-lg font-semibold text-black transition duration-300 hover:bg-yellow-600 focus:outline-none">
+              Get Started
+            </button>
           </div>
-          <p className="mb-2 text-lg text-gray-600">
-            Admission: Applications Opened
-          </p>
-          <p className="mb-6 text-lg font-bold text-gray-900">
-            Deadline: 8th July, 2024
-          </p>
-          <button className="w-full rounded-md bg-yellow-500 px-6 py-3 text-lg font-semibold text-black transition duration-300 hover:bg-yellow-600 focus:outline-none">
-            Get Started
+          <button
+            onClick={handleToggle}
+            className={`fixed left-0 top-1/2 -translate-y-1/2 transform rounded-r-md bg-white p-2 shadow-md transition-all duration-500 sm:hidden ${
+              isVisible
+                ? 'pointer-events-none translate-x-full opacity-0'
+                : 'translate-x-0 opacity-100'
+            }`}
+          >
+            <ChevronRight className="h-8 w-8 text-black" />
           </button>
         </div>
-        <button
-          onClick={handleToggle}
-          className={`absolute left-0 top-1/2 -translate-y-1/2 transform animate-pulse rounded-full bg-white p-2 transition-opacity duration-500 ${isVisible ? 'opacity-0' : 'opacity-100'} sm:hidden`}
-        >
-          <ChevronRight
-            className={`h-8 w-8 text-black transition-transform duration-500 ${isVisible ? 'rotate-180' : ''}`}
-          />
-        </button>
       </div>
     </section>
   )
--- src/components/newsandupcoming.tsx
+++ src/components/newsandupcoming.tsx
@@ -1,8 +1,10 @@
 'use client'
+import { Calendar, ChevronRight } from 'lucide-react'
 import React, { useState } from 'react'
 
+// Happenings (News) Component
 const Happenings = () => {
-  const events = [
+  const news = [
     {
       date: '9 Jun 2024',
       title:
@@ -12,28 +14,84 @@ const Happenings = () => {
     {
       date: '5th July 2024',
       title:
-        'In recognition of the contribution towards Industry Academia Community',
-      imageUrl: '/ssr2.png',
+        'We , the Fr. CRCE extend our heartfelt gratitude for your contributions and commitment. Your presence has made a lasting impact, and we wish you continued success and fulfillment in your future endeavors.Happy Farewell.',
+      imageUrl:
+        'https://media.licdn.com/dms/image/v2/D4D22AQE23ch8dhe_Lg/feedshare-shrink_1280/feedshare-shrink_1280/0/1725991344448?e=1729123200&v=beta&t=B5e6u3TOfU7tGEU-97QYKERb4zOgDvTaKAWGgRFSBqs',
     },
     {
-      date: '',
-      title: '',
-      imageUrl: '/ssr2.png',
+      date: '15 Aug 2024',
+      title:
+        'We hosted the 26th Edition of the FR CONCEICAO RODRIGUES MEMORIAL DEBATE (CRMD) on 23rd & 24th August 24! This prestigious event had 32 formidable teams from across the nation, showcasing their unparalleled debating prowess. 🌟',
+      imageUrl:
+        'https://media.licdn.com/dms/image/v2/D4D22AQH89X7z1KvIag/feedshare-shrink_2048_1536/feedshare-shrink_2048_1536/0/1725085515956?e=1729123200&v=beta&t=qwsxm47dvjx64PFquyqQCZU2acmAUXqMQ7VIEZjQmL4',
     },
     {
-      date: '',
-      title: '',
-      imageUrl: '/ssr2.png',
+      date: '25 Sep 2024',
+      title:
+        'We are excited to share the launch of the "Startup Clinic", an incubation center at Fr. Conceicao Rodrigues College of Engineering, powered by the University of Mumbai IDEAS Foundation!',
+      imageUrl:
+        'https://media.licdn.com/dms/image/v2/D4D22AQEiVTX3dJCMIg/feedshare-shrink_2048_1536/feedshare-shrink_2048_1536/0/1725335957750?e=1729123200&v=beta&t=1L8QNSbxfbQa5qVM1yBlr5W52l2Bip3jDtVvFVzyuJU',
     },
     {
-      date: '',
-      title: '',
-      imageUrl: '/ssr2.png',
+      date: '10 Oct 2024',
+      title:
+        'We are deeply grateful to our alumni for their invaluable support and guidance to our students.',
+      imageUrl:
+        'https://media.licdn.com/dms/image/v2/D4D22AQFyFHCAIdMhbg/feedshare-shrink_2048_1536/feedshare-shrink_2048_1536/0/1719188271605?e=1729123200&v=beta&t=74ejCueEMbg5Mf9FDQnZ5mGKSSVTVPtlcUz-7FlBx2w',
     },
     {
-      date: '',
-      title: '',
-      imageUrl: '/ssr2.png',
+      date: '1 Nov 2024',
+      title:
+        'Our upcoming TEDxCRCE event (8th Edition) scheduled for 3rd February 24, themed "Cracks in the Canvas."',
+      imageUrl:
+        'https://media.licdn.com/dms/image/v2/D5622AQFesUJ0xZCX9g/feedshare-shrink_1280/feedshare-shrink_1280/0/1706547776154?e=1729123200&v=beta&t=15Zt-AW17kdy9lrTzbP0MZgFGmQJVxXaW3CijLIL7y8',
+    },
+  ]
+
+  return (
+    <div className="mt-5 w-full bg-white py-8">
+      <div className="container mx-auto max-w-6xl px-4">
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          {news.map((item, index) => (
+            <div
+              key={index}
+              className="group h-64 overflow-hidden rounded-lg bg-white shadow-sm transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-md"
+              style={{
+                backgroundImage: `url(${item.imageUrl})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }}
+            >
+              <div className="flex h-full flex-col justify-end bg-black bg-opacity-50 p-4 transition-all duration-300 group-hover:bg-opacity-0">
+                <p className="mb-2 text-sm text-gray-200 transition-colors duration-300 group-hover:text-white">
+                  {item.date}
+                </p>
+                <h3 className="line-clamp-2 text-base font-semibold text-white transition-all duration-300 group-hover:text-lg group-hover:text-blue-200">
+                  {item.title}
+                </h3>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+// Events Component
+const EventCards = () => {
+  const events = [
+    {
+      date: '1 Dec 2024',
+      title: 'Annual Sports Meet 2024',
+      imageUrl:
+        'https://media.licdn.com/dms/image/v2/D4D22AQGvdYSDWKmBZQ/feedshare-shrink_2048_1536/feedshare-shrink_2048_1536/0/1703087979244?e=1729123200&v=beta&t=dAf_ynCo_eB5O0VgGRiDXFQBvGOaEMgNOdvN5bLVpaQ',
+    },
+    {
+      date: '10 sep 2024',
+      title: 'Technical Fest - FrCRCE',
+      imageUrl:
+        'https://media.licdn.com/dms/image/v2/D4D22AQGFyBRVukWOFg/feedshare-shrink_2048_1536/feedshare-shrink_2048_1536/0/1725084643117?e=1729123200&v=beta&t=RDxpL6r0YSs7f1KdqIpJM94JN-VQweaAp38rFke2Irg',
     },
   ]
 
@@ -67,6 +125,51 @@ const Happenings = () => {
   )
 }
 
+const DeadlineCards = () => {
+  const deadlines = [
+    {
+      date: '15 Dec 2024',
+      title: 'Submission of Project Proposals',
+    },
+    {
+      date: '25 Dec 2024',
+      title: 'Final Exams Registration Deadline',
+    },
+  ]
+
+  return (
+    <div className="mt-5 w-full bg-white py-8">
+      <div className="container mx-auto max-w-6xl px-4">
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          {deadlines.map((deadline, index) => (
+            <div
+              key={index}
+              className="group h-64 overflow-hidden rounded-lg border border-gray-200 bg-white shadow-sm transition-all duration-300 ease-in-out hover:shadow-md"
+            >
+              <div className="flex h-full flex-col justify-between p-6">
+                <div>
+                  <div className="mb-4 flex items-center">
+                    <Calendar className="mr-2 h-5 w-5 text-blue-600" />
+                    <p className="text-sm font-semibold text-blue-600">
+                      {deadline.date}
+                    </p>
+                  </div>
+                  <h3 className="mb-4 text-xl font-bold leading-tight text-gray-800">
+                    {deadline.title}
+                  </h3>
+                </div>
+                <button className="mt-4 flex items-center justify-center rounded-md bg-blue-600 px-4 py-2 text-sm font-medium text-white transition-colors duration-300 hover:bg-blue-700">
+                  Learn More
+                  <ChevronRight className="ml-2 h-4 w-4" />
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
 const Newsandupcoming = () => {
   const [activeTab, setActiveTab] = useState('news')
 
@@ -101,8 +204,8 @@ const Newsandupcoming = () => {
 
       <div id="contentArea" className="mt-6 text-center text-lg">
         {activeTab === 'news' && <Happenings />}
-        {activeTab === 'events' && <p>Events</p>}
-        {activeTab === 'deadlines' && <p>Deadlines</p>}
+        {activeTab === 'events' && <EventCards />}
+        {activeTab === 'deadlines' && <DeadlineCards />}
       </div>
     </div>
   )
