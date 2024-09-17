90c207da71c8e02aa57d353185cb0b77afa81f60
arshdeepkaur22 <arshd33p.kaur05@gmail.com>
2024-09-17 12:04:47 +0530

    changes

--- src/app/AboutDepartment/MechanicalDepartment/infra.tsx
+++ src/app/AboutDepartment/MechanicalDepartment/infra.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
-
 import { Zilla_Slab } from 'next/font/google'
-import Link from 'next/link'
 
-// the font for main headings
 const zilla = Zilla_Slab({
   weight: ['300', '500', '700'],
   style: ['normal', 'italic'],
@@ -12,77 +9,226 @@ const zilla = Zilla_Slab({
   display: 'swap',
 })
 
-interface InfrasProps {
-  title: string
-  description: string
-  imageUrl: string
-}
-
-const Infras: React.FC<InfrasProps> = ({ title, description, imageUrl }) => {
+const Infrastructure = () => {
   return (
     <div className="container mx-auto p-6">
-      <h2
-        className={`${zilla.className} mb-8 text-center text-4xl font-bold text-blue-950 md:text-5xl lg:text-6xl`}
+      <h1
+        className={`${zilla.className} mb-12 text-center text-5xl font-bold text-blue-900`}
       >
-        Infrastructure
-      </h2>
+        Mechanical Engineering Infrastructure
+      </h1>
 
-      {/* Section for Department Details */}
-      <section className="mb-8 rounded-lg bg-gray-100 p-8 shadow-lg">
-        <h2 className="mb-4 text-2xl font-semibold text-blue-800">
-          Department Details
+      <div className="rounded-lg bg-white p-8 shadow-lg">
+        <h2
+          className={`${zilla.className} mb-12 text-center text-2xl font-bold text-blue-900`}
+        >
+          Laboratories and Facilities
         </h2>
-        <p className="mb-6 leading-relaxed text-gray-700">
-          The Department is headed by Dr. Sapna Prabhu, who has been
-          instrumental in leading several initiatives aimed at modernizing our
-          infrastructure and curriculum.
-          {/* Add more content here */}
-        </p>
-        <div className="flex justify-center">
-          <div className="group relative">
-            <Image
-              src="/ssr.png"
-              alt="Department Head"
-              height={300}
-              width={300}
-              className="transform rounded-lg transition-transform duration-300 group-hover:scale-110"
-            />
-            <div className="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 group-hover:opacity-40"></div>
+
+        <div className="space-y-8">
+          <div>
+            <h3
+              className={`${zilla.className} mb-6 text-center text-2xl font-bold text-blue-900`}
+            >
+              Mechanisms and Machines Laboratory
+            </h3>
+            <div className="md:flex md:space-x-4">
+              <div className="md:w-1/2">
+                <p className="mb-4 text-gray-700">
+                  This laboratory facilitates experimentation related to
+                  mechanics with various apparatuses such as Force Table, Coil
+                  Spring, Bell Crank Lever, Friction Slide, Simple Beam, and
+                  Collision of Elastic Bodies. It also includes equipment for
+                  dynamics of machines experiments like static &amp; dynamic
+                  balancing apparatus, Whirling Speed of shaft, Gyroscope,
+                  Governor, Cam analysis apparatus, Coriolis Component of
+                  acceleration, Piezoelectric Vibrometer and Vibration
+                  Apparatus.
+                </p>
+              </div>
+              <div className="mt-4 md:mt-0 md:w-1/2">
+                <Image
+                  src="/inframech/dom.jpg"
+                  alt="Mechanisms and Machines Laboratory"
+                  width={400}
+                  height={300}
+                  className="rounded-lg"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <h3
+              className={`${zilla.className} mb-6 text-center text-2xl font-bold text-blue-900`}
+            >
+              CAD/CAM Laboratory
+            </h3>
+            <div className="md:flex md:space-x-4">
+              <div className="md:w-1/2">
+                <p className="mb-4 text-gray-700">
+                  This laboratory is well-equipped with state-of-art
+                  computational facilities including High-End Graphic
+                  Workstations. It features 3D Modeling and Analysis Software
+                  such as Ansys, NX11, Solidworks, and Autocad. The lab also
+                  includes a 3D Printer for rapid prototyping.
+                </p>
+              </div>
+              <div className="mt-4 md:mt-0 md:w-1/2">
+                <Image
+                  src="/inframech/cadcam.jpg"
+                  alt="CAD/CAM Laboratory"
+                  width={400}
+                  height={300}
+                  className="rounded-lg"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <h3
+              className={`${zilla.className} mb-6 text-center text-2xl font-bold text-blue-900`}
+            >
+              HVAC Laboratory
+            </h3>
+            <div className="md:flex md:space-x-4">
+              <div className="md:w-1/2">
+                <p className="mb-4 text-gray-700">
+                  This laboratory facilitates experimentation related to HVAC
+                  using cooling tower, ice plant, open-type air conditioning
+                  test rig and vapour absorption refrigeration test rig.
+                  Students can gain hands-on experience with various HVAC
+                  systems and principles.
+                </p>
+              </div>
+              <div className="mt-4 md:mt-0 md:w-1/2">
+                <Image
+                  src="/inframech/havac.jpg"
+                  alt="HVAC Laboratory"
+                  width={400}
+                  height={300}
+                  className="rounded-lg"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <h3
+              className={`${zilla.className} mb-6 text-center text-2xl font-bold text-blue-900`}
+            >
+              Turbomachinery Laboratory
+            </h3>
+            <div className="md:flex md:space-x-4">
+              <div className="md:w-1/2">
+                <p className="mb-4 text-gray-700">
+                  This laboratory facilitates experimentation related to
+                  Turbomachinery using Pelton Turbine, Francis Turbine,
+                  Venturimeter, Impact of Jet Apparatus, Air compressor,
+                  Metacentric Height and Bernoulli's Apparatus. Students can
+                  study and experiment with various types of turbines and fluid
+                  machinery.
+                </p>
+              </div>
+              <div className="mt-4 md:mt-0 md:w-1/2">
+                <Image
+                  src="/inframech/turbo.jpg"
+                  alt="Turbomachinery Laboratory"
+                  width={400}
+                  height={300}
+                  className="rounded-lg"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <h3
+              className={`${zilla.className} mb-6 text-center text-2xl font-bold text-blue-900`}
+            >
+              FE Workshop
+            </h3>
+            <div className="md:flex md:space-x-4">
+              <div className="md:w-1/2">
+                <p className="mb-4 text-gray-700">
+                  The First Year Engineering Workshop includes facilities such
+                  as Carpentry, Fitting and State-of-Art Welding Machines. This
+                  workshop provides students with hands-on experience in basic
+                  engineering skills and manufacturing processes.
+                </p>
+              </div>
+              <div className="mt-4 md:mt-0 md:w-1/2">
+                <Image
+                  src="/inframech/feworkshop.jpg"
+                  alt="FE Workshop"
+                  width={400}
+                  height={300}
+                  className="rounded-lg"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <h3
+              className={`${zilla.className} mb-6 text-center text-2xl font-bold text-blue-900`}
+            >
+              Machine Shop
+            </h3>
+            <div className="md:flex md:space-x-4">
+              <div className="md:w-1/2">
+                <p className="mb-4 text-gray-700">
+                  The Machine shop is equipped with adequate Conventional
+                  Lathes, Shaper, Milling Machine, Drilling Machine, Grinder,
+                  Universal Milling Machine (Becker) along with Modern
+                  Production Grade CNC Turning and Milling Machines. This
+                  facility allows students to gain practical experience in
+                  various machining processes.
+                </p>
+              </div>
+              <div className="mt-4 md:mt-0 md:w-1/2">
+                <Image
+                  src="/inframech/mahineshop.jpg"
+                  alt="Machine Shop"
+                  width={400}
+                  height={300}
+                  className="rounded-lg"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <h3
+              className={`${zilla.className} mb-6 text-center text-2xl font-bold text-blue-900`}
+            >
+              PG Laboratory (Project Based Learning)
+            </h3>
+            <div className="md:flex md:space-x-4">
+              <div className="md:w-1/2">
+                <p className="mb-4 text-gray-700">
+                  This laboratory provides an environment for projects based
+                  learning and is equipped with Modelling Softwares, MATLAB and
+                  other Statistical Analysis Freewares. It supports postgraduate
+                  students in their research and project work.
+                </p>
+              </div>
+              <div className="mt-4 md:mt-0 md:w-1/2">
+                <Image
+                  src="/inframech/msmlab.jpg"
+                  alt="PG Laboratory"
+                  width={400}
+                  height={300}
+                  className="rounded-lg"
+                />
+              </div>
+            </div>
           </div>
         </div>
-      </section>
-
-      {/* Section for Laboratory Details */}
-      <section className="mb-8 rounded-lg bg-gray-100 p-8 shadow-lg">
-        <h2 className="mb-4 text-2xl font-semibold text-blue-800">
-          Laboratory Details
-        </h2>
-        <h3 className="mb-2 text-xl font-semibold text-blue-700">
-          Computing Laboratory - I
-        </h3>
-        <ul className="mb-6 list-disc pl-6 text-gray-700">
-          <li>Equipped with 21 desktop computers, each with 16GB RAM...</li>
-          <li>Software setup includes the latest versions of...</li>
-          {/* Add more details here */}
-        </ul>
-
-        <div className="flex justify-center">
-          <div className="group relative">
-            <Image
-              src="/ssr.png"
-              alt="Computing Lab"
-              height={300}
-              width={300}
-              className="transform rounded-lg transition-transform duration-300 group-hover:scale-110"
-            />
-            <div className="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 group-hover:opacity-40"></div>
-          </div>
-        </div>
-      </section>
-
-      {/* Add another section for Computing Laboratory - II similarly */}
+      </div>
     </div>
   )
 }
 
-export default Infras
+export default Infrastructure
