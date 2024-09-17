e44474a80ac6fd5042991a0885ea0a63de6d8c25
arshdeepkaur22 <arshd33p.kaur05@gmail.com>
2024-09-17 11:35:57 +0530

    somemore

--- src/app/AboutDepartment/EcsDepartment/infra.tsx
+++ src/app/AboutDepartment/EcsDepartment/infra.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
-
 import { Zilla_Slab } from 'next/font/google'
-import Link from 'next/link'
 
-// the font for main headings
 const zilla = Zilla_Slab({
   weight: ['300', '500', '700'],
   style: ['normal', 'italic'],
@@ -12,77 +9,219 @@ const zilla = Zilla_Slab({
   display: 'swap',
 })
 
-interface InfrasProps {
-  title: string
-  description: string
-  imageUrl: string
-}
-
-const Infras: React.FC<InfrasProps> = ({ title, description, imageUrl }) => {
+const Infrastructure = () => {
   return (
     <div className="container mx-auto p-6">
-      <h2
-        className={`${zilla.className} mb-8 text-center text-4xl font-bold text-blue-950 md:text-5xl lg:text-6xl`}
+      <h1
+        className={`${zilla.className} mb-12 text-center text-5xl font-bold text-blue-900`}
       >
         Infrastructure
-      </h2>
+      </h1>
 
-      {/* Section for Department Details */}
-      <section className="mb-8 rounded-lg bg-gray-100 p-8 shadow-lg">
-        <h2 className="mb-4 text-2xl font-semibold text-blue-800">
-          Department Details
+      <div className="rounded-lg bg-white p-8 shadow-lg">
+        <h2
+          className={`${zilla.className} mb-12 text-center text-2xl font-bold text-blue-900`}
+        >
+          Computing and Hardware Laboratories
         </h2>
-        <p className="mb-6 leading-relaxed text-gray-700">
-          The Department is headed by Dr. Sapna Prabhu, who has been
-          instrumental in leading several initiatives aimed at modernizing our
-          infrastructure and curriculum.
-          {/* Add more content here */}
-        </p>
-        <div className="flex justify-center">
-          <div className="group relative">
-            <Image
-              src="/ssr.png"
-              alt="Department Head"
-              height={300}
-              width={300}
-              className="transform rounded-lg transition-transform duration-300 group-hover:scale-110"
-            />
-            <div className="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 group-hover:opacity-40"></div>
+
+        <div className="space-y-8">
+          <div>
+            <h3
+              className={`${zilla.className} mb-6 text-center text-2xl font-bold text-blue-900`}
+            >
+              Computing Laboratory - I
+            </h3>
+            <div className="md:flex md:space-x-4">
+              <div className="md:w-1/2">
+                <p className="mb-4 text-gray-700">
+                  This laboratory caters to the subjects Web Technologies,
+                  Software Engineering, Embedded Systems, Computer Organization
+                  and IOT. It is equipped with 21 desktop computers with various
+                  embedded system software and kits. A Remotelab facility
+                  enables students to access FPGA and ARM kits remotely.
+                </p>
+                <p className="mb-4 text-gray-700">
+                  Available software: Proteus, Keil, Mplab, TASM, SSI, AVR
+                  studio, Winpic, RTOS, PSOC Creator Software, Star UML.
+                </p>
+                <p className="mb-4 text-gray-700">
+                  Available kits: 8085, 8086, 8051 kits, ARM kits, ARM-Cortex,
+                  AVR kits, Universal trainer kit (ARM+PIC), PSOC kits, Stepper
+                  motor controller cards.
+                </p>
+              </div>
+              <div className="mt-4 md:mt-0 md:w-1/2">
+                <Image
+                  src="/infraecs/311-1_1.jpg"
+                  alt="Computing Laboratory - I"
+                  width={400}
+                  height={400}
+                  className="rounded-lg"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <h3
+              className={`${zilla.className} mb-6 text-center text-2xl font-bold text-blue-900`}
+            >
+              Computing Laboratory - II
+            </h3>
+            <div className="md:flex md:space-x-4">
+              <div className="md:w-1/2">
+                <p className="mb-4 text-gray-700">
+                  This laboratory focuses on VLSI Design, Image Processing, and
+                  simulation experiments of Digital and Analog circuits. It is
+                  equipped with Sun Solaris Workstations and additional machines
+                  with VHDL software for digital circuit simulation and
+                  synthesis.
+                </p>
+                <p className="mb-4 text-gray-700">
+                  Students gain hands-on experience with SILVACO (process
+                  simulation), MAGIC (layout design), LabView (process
+                  simulation), and SPICE (circuit simulation).
+                </p>
+              </div>
+              <div className="mt-4 md:mt-0 md:w-1/2">
+                <Image
+                  src="/infraecs/ee-nl.jpg"
+                  alt="Computing Laboratory - II"
+                  width={400}
+                  height={400}
+                  className="rounded-lg"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <h3
+              className={`${zilla.className} mb-6 text-center text-2xl font-bold text-blue-900`}
+            >
+              Computing Laboratory - III
+            </h3>
+            <div className="md:flex md:space-x-4">
+              <div className="md:w-1/2">
+                <p className="mb-4 text-gray-700">
+                  This laboratory supports courses in Filter Theory, Wireless
+                  Communication, Signals and Systems, Data Communication, and
+                  Advanced Communication Theory. It features 20 Sun-Ray
+                  workstations with Matlab software for communication and signal
+                  processing simulations.
+                </p>
+                <p className="mb-4 text-gray-700">
+                  Equipment includes an Antenna trainer, TV trainer, Spectrum
+                  Analyzer, and Digital Signal Processing kits with TMS320C6713
+                  DSP processors.
+                </p>
+              </div>
+              <div className="mt-4 md:mt-0 md:w-1/2">
+                <Image
+                  src="/infraecs/ee-cl.jpg"
+                  alt="Computing Laboratory - III"
+                  width={400}
+                  height={400}
+                  className="rounded-lg"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <h3
+              className={`${zilla.className} mb-6 text-center text-2xl font-bold text-blue-900`}
+            >
+              Hardware Laboratory - I
+            </h3>
+            <div className="md:flex md:space-x-4">
+              <div className="md:w-1/2">
+                <p className="mb-4 text-gray-700">
+                  Used for practical courses in Electronic Devices and Circuits,
+                  and Power Electronics. It is equipped with various Power
+                  Electronics kits including Choppers, Inverters, D.C. drives,
+                  A.C. drives, and UPS systems.
+                </p>
+              </div>
+              <div className="mt-4 md:mt-0 md:w-1/2">
+                <Image
+                  src="/infraecs/ee-cl.jpg"
+                  alt="Hardware Laboratory - I"
+                  width={800}
+                  height={400}
+                  className="rounded-lg"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <h3
+              className={`${zilla.className} mb-6 text-center text-2xl font-bold text-blue-900`}
+            >
+              Hardware Laboratory - II
+            </h3>
+            <div className="md:flex md:space-x-4">
+              <div className="md:w-1/2">
+                <p className="mb-4 text-gray-700">
+                  This lab supports courses in Instrumentation Systems, Control
+                  Systems, Robotics, and Mechatronics. It features PID
+                  controller and PLC kits for Process Instrumentation
+                  experiments, as well as equipment for experiments with Servo
+                  motors and Synchros in Control Systems.
+                </p>
+              </div>
+              <div className="mt-4 md:mt-0 md:w-1/2">
+                <Image
+                  src="/infraecs/ee-acl.jpg"
+                  alt="Hardware Laboratory - II"
+                  width={800}
+                  height={400}
+                  className="rounded-lg"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <h3
+              className={`${zilla.className} mb-6 text-center text-2xl font-bold text-blue-900`}
+            >
+              Electrical Lab
+            </h3>
+            <div className="md:flex md:space-x-4">
+              <div className="md:w-1/2">
+                <p className="mb-4 text-gray-700">
+                  This lab serves courses in Basic Electrical Engineering,
+                  Industrial Electronics, and Electrical Wiring Workshop. It is
+                  equipped for experiments on Electrical Machines and
+                  Transformers, featuring 3-phase Induction motors, DC motors,
+                  Synchronous motors, Motor-Generator sets, and various
+                  measurement instruments.
+                </p>
+                <p className="mb-4 text-gray-700">
+                  Equipment includes Digital Storage Oscilloscopes,
+                  Multi-Meters, 3-phase Rectifier Units, Function Generators, DC
+                  power supplies, Digital Tachometers, and specialized trainer
+                  kits like LVDT and RTD.
+                </p>
+              </div>
+              <div className="mt-4 md:mt-0 md:w-1/2">
+                <Image
+                  src="/infraecs/ee-elc.jpg"
+                  alt="Electrical Lab"
+                  width={800}
+                  height={400}
+                  className="rounded-lg"
+                />
+              </div>
+            </div>
           </div>
         </div>
-      </section>
-
-      {/* Section for Laboratory Details */}
-      <section className="mb-8 rounded-lg bg-gray-100 p-8 shadow-lg">
-        <h2 className="mb-4 text-2xl font-semibold text-blue-800">
-          Laboratory Details
-        </h2>
-        <h3 className="mb-2 text-xl font-semibold text-blue-700">
-          Computing Laboratory - I
-        </h3>
-        <ul className="mb-6 list-disc pl-6 text-gray-700">
-          <li>Equipped with 21 desktop computers, each with 16GB RAM...</li>
-          <li>Software setup includes the latest versions of...</li>
-          {/* Add more details here */}
-        </ul>
-
-        <div className="flex justify-center">
-          <div className="group relative">
-            <Image
-              src="/ssr.png"
-              alt="Computing Lab"
-              height={300}
-              width={300}
-              className="transform rounded-lg transition-transform duration-300 group-hover:scale-110"
-            />
-            <div className="absolute inset-0 bg-black opacity-0 transition-opacity duration-300 group-hover:opacity-40"></div>
-          </div>
-        </div>
-      </section>
-
-      {/* Add another section for Computing Laboratory - II similarly */}
+      </div>
     </div>
   )
 }
 
-export default Infras
+export default Infrastructure
